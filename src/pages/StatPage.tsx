7e65ea46afec4bf0d281c7633dc3a4b7a71bc17d
Mamun Reza <mamun.reza09@gmail.com>
2024-09-10 18:07:06 +0300

    OMS-20 more cleanups (#24)
    * OMS-20 more cleanups
    
    * OMS-20 correction
    
    ---------
    
    Co-authored-by: Mamun Reza <Muhammad.Reza@energia.ee>

--- src/pages/StatPage.tsx
+++ src/pages/StatPage.tsx
@@ -1,6 +1,5 @@
 // pages/TodoPage.tsx
 import React from 'react';
-import '../components/Todo/styles.css'; // Import the styles
 import ActivityFrequencyGraph from '../components/Activity/ActivityFrequenceyGraph';
 
 const StatPage: React.FC = () => {
